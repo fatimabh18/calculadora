463c79662abb26e302d3feb25b436127f81975eb
Fatima <fatimabha99@gmail.com>
2022-02-24 10:13:11 +0100

    switch

--- Program.cs
+++ Program.cs
@@ -4,44 +4,49 @@ class program
     static void Main(string[] args)
     {
         string opcion = "";
-        int numero1;
-        int numero2;
+        int numero1 = 0;
+        int numero2 = 0;
         var calculadora = new Calculadora();
+
         while (opcion != "5")
         {
-         Console.WriteLine("******MENU******");
-         Console.WriteLine("\n¿Que operacion desea realizar?");
-         Console.WriteLine("Opcion 1:Sumar");
-         Console.WriteLine("Opcion2:Restar");
-         Console.WriteLine("Opcion3:Multiplicar");
-         Console.WriteLine("Opcion4:Dividir");
-         Console.WriteLine("Opcion5:Salir");
-         Console.WriteLine("\nElija una opcion");
-         opcion = Console.ReadLine();
-            if (opcion == "1" || opcion == "2" || opcion == "3" || opcion == "4")
+            Console.WriteLine("******MENU******");
+            Console.WriteLine("\n¿Que operacion desea realizar?");
+            Console.WriteLine("Opcion 1:Sumar");
+            Console.WriteLine("Opcion2:Restar");
+            Console.WriteLine("Opcion3:Multiplicar");
+            Console.WriteLine("Opcion4:Dividir");
+            Console.WriteLine("Opcion5:Salir");
+            Console.WriteLine("\nElija una opcion");
+            opcion = Console.ReadLine();
+            switch (opcion)
             {
-                Console.WriteLine("Introduce un numero");
-                numero1 = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Introduce un segundo número");
-                numero2 = Int32.Parse(Console.ReadLine());
-                if (opcion == "1")
-                {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                    Console.WriteLine("Introduce un numero");
+                    numero1 = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("Introduce un segundo número");
+                    numero2 = Int32.Parse(Console.ReadLine());
+                    break;
+            }
+            switch (opcion)
+            {
+                case "1":
                     calculadora.Suma(numero1, numero2);
-                }
-                if (opcion == "2")
-                {
+                    break;
+                case "2":
                     calculadora.Resta(numero1, numero2);
-                }
-                if (opcion == "3")
-                {
+                    break;
+                case "3":
                     calculadora.Multiplicación(numero1, numero2);
-                }
-                if (opcion == "4")
-                {
+                    break;
+                case "4":
                     calculadora.División(numero1, numero2);
-                }
-            } 
-        }  
+                    break;
+            }
+        }   
     }
 }
 
