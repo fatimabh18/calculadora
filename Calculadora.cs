a1c836bfe12dbf63795f0f2e95a080f9bf78890f
Carlos <crealmadrid618@gmail.com>
2022-02-24 10:23:59 +0100

    Dev calculadora

--- Calculadora.cs
+++ Calculadora.cs
@@ -2,28 +2,32 @@ using System;
 
 public class Calculadora
 {
-    public void Suma(int sumando1, int sumando2)
+    public static int Suma(int sumando1, int sumando2)
     {
         int resultado;
         resultado = sumando1 + sumando2;
         Console.WriteLine("El resultado de la suma es "+resultado);
+        return(resultado);
     }
-    public void Resta(int minuendo, int sustraendo)
+    public static int Resta(int minuendo, int sustraendo)
     {
         int resultado;
         resultado = minuendo- sustraendo;
         Console.WriteLine("El resultado de la resta es: " + resultado);
+        return resultado;
     }
-    public void Multiplicación(int multiplicador1, int multiplicado2)
+    public static int Multiplicación(int multiplicador1, int multiplicado2)
     {
         int resultado;
         resultado = multiplicador1 * multiplicado2;
         Console.WriteLine("El resultado de la multiplicación es: " + resultado);
+        return (resultado);
     }
-    public void División(int dividendo, int divisor)
+    public static int División(int dividendo, int divisor)
     {
         int resultado;
         resultado = dividendo / divisor;
         Console.WriteLine("El resultado de la división es: " + resultado);
+        return (resultado);
     }
 }
