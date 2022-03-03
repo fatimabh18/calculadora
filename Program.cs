48143986429feeffe56c80c3e05ae739c15eda58
Fatima <fatimabha99@gmail.com>
2022-03-03 10:19:15 +0100

    enumerados y namespace

--- Program.cs
+++ Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using Clase.Calculadora;
+
+enum opcionUsuario
+{
+   opcion = 1,
+   opcion2 = 2,
+   opcion3 = 3,
+   opcion4 = 4,
+}
 class program
 {
     static void Main(string[] args)
     {
-        string opcion = "";
+        var calculadora = new Calculadora();
         int numero1 = 0;
         int numero2 = 0;
-        var calculadora = new Calculadora();
 
-        while (opcion != "5")
+        while ((int)opcionUsuario.opcion != 5)
         {
             Console.WriteLine("******MENU******");
             Console.WriteLine("\n¿Que operacion desea realizar?");
@@ -18,31 +26,33 @@ class program
             Console.WriteLine("Opcion4:Dividir");
             Console.WriteLine("Opcion5:Salir");
             Console.WriteLine("\nElija una opcion");
-            opcion = Console.ReadLine();
+            opcionUsuario opcion = (opcionUsuario) Convert.ToInt32(Console.ReadLine());
             switch (opcion)
             {
-                case "1":
-                case "2":
-                case "3":
-                case "4":
+                case opcionUsuario.opcion:
+                case opcionUsuario.opcion2:
+                case opcionUsuario.opcion3:
+                case opcionUsuario.opcion4:
                     Console.WriteLine("Introduce un numero");
-                    numero1 = Int32.Parse(Console.ReadLine());
+                    numero1 = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("Introduce un segundo número");
-                    numero2 = Int32.Parse(Console.ReadLine());
+                    numero2 = Convert.ToInt32(Console.ReadLine());
                     break;
+                default:
+                    return;
             }
             switch (opcion)
             {
-                case "1":
+                case opcionUsuario.opcion:
                     Calculadora.Suma(numero1 , numero2);
                     break;
-                case "2":
+                case opcionUsuario.opcion2:
                     Calculadora.Resta(numero1, numero2);
                     break;
-                case "3":
+                case opcionUsuario.opcion3:
                     Calculadora.Multiplicación(numero1, numero2);
                     break;
-                case "4":
+                case opcionUsuario.opcion4:
                     Calculadora.División(numero1, numero2);
                     break;
             }
