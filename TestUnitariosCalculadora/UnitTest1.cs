8fdde91792cd51508a58ca9dcc5f450221753740
Fatima <fatimabha99@gmail.com>
2022-03-24 09:58:50 +0100

    Test Operación Suma

--- TestUnitariosCalculadora/UnitTest1.cs
+++ TestUnitariosCalculadora/UnitTest1.cs
@@ -1,13 +1,21 @@
 using Xunit;
+using Clase.Calculadora;
 
-namespace TestUnitariosCalculadora
+namespace TestUnitariosCalculador
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Test_Operacion_Suma()
         {
-
+            // arrange
+            int sumando1 = 2;
+            int sumando2 = 2;
+            // act
+            Calculadora calculadora = new Calculadora();
+            int resultado = calculadora.Suma(sumando1, sumando2);
+            // asert
+            Assert.Equal(4, resultado);
         }
     }
 }
