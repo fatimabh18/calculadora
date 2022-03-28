3b01b17ed4cb0f77defe33578a46eac19e851abd
Fatima <fatimabha99@gmail.com>
2022-03-28 14:05:21 +0200

    TestMultiplicacion

--- TestUnitariosCalculadora/CalculadoraUnitTest.cs
+++ TestUnitariosCalculadora/CalculadoraUnitTest.cs
@@ -17,5 +17,17 @@ namespace TestUnitariosCalculador
             // asert
             Assert.Equal(4, resultado);
         }
+        [Fact]
+        public void Test_Operacion_Multiplicacion()
+        {
+            // arrange
+            int multiplicador1 = 2;
+            int multiplicador2 = 5;
+            // act
+            Calculadora calculadora = new Calculadora();
+            int resultado = calculadora.Multiplicacion(multiplicador1, multiplicador2);
+            // asert
+            Assert.Equal(10, resultado);
+        }
     }
 }
